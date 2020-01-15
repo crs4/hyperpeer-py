848715080852ed1c2ac7e580b4d2774353d794dd
Jose Saenz <josefsaenz@gmail.com>
2020-01-15 17:11:49 +0100

    updated package info and distribution files

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hyperpeer", # Replace with your own username
-    version="0.0.1",
+    name="hyperpeer-py",
+    version="0.1.0",
     author="Jose F. Saenz-Cogollo",
     author_email="jsaenz@crs4.it",
     description="Python module for implementing media servers or backend peers in applications based on Hyperpeer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="http://156.148.14.162/icare2/hyperpeer-py",
+    url="https://github.com/crs4/hyperpeer-py",
     packages=setuptools.find_packages(),
     install_requires=['aiortc==0.9.22', 'websockets'],
     classifiers=[
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
